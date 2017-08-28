2b79660c7718267c76f1b1d216ab7ec2cb59fc0c
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-08-28 18:30:19 -0300

    Adding practical values from exercise 3

--- rel1.py
+++ rel1.py
@@ -13,11 +13,11 @@ def theory(inlet):
     if 'l' in inlet:
         l = 2 * cmath.pi * 60 * inlet['l']
     if 'c' in inlet:
-        c = 1 / (2 * cmath.pi * 60 * inlet['l'])
+        c = 1 / (2 * cmath.pi * 60 * inlet['c'])
 
     z = r + 1j * l
     if c > 0:
-        z += 1j / c
+        z += -1j * c
 
     i = v / z
     s = v * star(i)
@@ -26,6 +26,10 @@ def theory(inlet):
     q = s.imag
     pf = p / cmath.sqrt(p**2 + q**2)
 
+    outlet['v'] = v
+    outlet['r'] = r
+    outlet['l'] = l
+    outlet['c'] = c
     outlet['z'] = z
     outlet['i'] = i
     outlet['s'] = s
@@ -34,11 +38,25 @@ def theory(inlet):
     outlet['pf'] = pf
     return outlet
 
+def practice(inlet):
+    outlet = { }
+    v = inlet['v']
+    w = inlet['w']
+    i = inlet['i']
+
+    # TODO Understand what W, S, P and Q are. This still is really fuzzy.
+
+    outlet['v'] = v
+    outlet['w'] = w
+    outlet['i'] = i
+
+    return outlet
+
 def display(what, data):
     print('--- # '+ what)
     for key in data:
         it = complex(data[key])
-        line = '%s = %.2f + j%.2f' % (key, it.real, it.imag)
+        line = '%s = %.6f + j%.6f' % (key, it.real, it.imag)
         print(line)
     print('...')
 
@@ -47,4 +65,15 @@ if __name__ == '__main__':
         'v': 200,
         'r': 485,
         'l': 0.585
+    }))
+    display('Pratica 3', practice({
+        'v': 200.1,
+        'w': 85,
+        'i': 0.754
+    }))
+    display('Ensaio 4', theory({
+        'v': 200,
+        'r': 485,
+        'l': 0.585,
+        'c': 12e-6
     }))
